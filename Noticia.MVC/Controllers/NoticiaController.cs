16caf9aff3b77c5f736af4b3488a40d51436ff6e
lucastteles <lucasteles_ccb@yahoo.com.br>
2022-08-23 11:54:26 -0300

    Adicionando Views

---
+++ Noticia.MVC/Controllers/NoticiaController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Noticias.Application.Interface;
+using Noticias.Application.ViewModel;
+using System;
+using System.Threading.Tasks;
+
+namespace Noticias.MVC.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        private readonly INoticiaApplication _noticaApplication;
+
+        public NoticiaController(INoticiaApplication noticiaApplication)
+        {
+            _noticaApplication = noticiaApplication;
+        }
+        // GET: NoticiaController
+        public async Task<IActionResult> Index()
+        {
+            var noticia = await _noticaApplication.ObterTodasNoticias();
+
+            return View(noticia);
+        }
+
+        // GET: NoticiaController/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var noticia = await _noticaApplication.ObterNoticiaPorId(id);
+
+            return View(noticia);
+        }
+
+        // GET: NoticiaController/Create
+        public async Task<IActionResult> Create()
+        {
+           
+            return View();
+        }
+
+        // POST: NoticiaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(NoticiaViewModel noticiaVM)
+        {
+            try
+            {
+                await _noticaApplication.AdicionarNoticia(noticiaVM);
+                ViewBag.Mensagem = $"{noticiaVM.Titulo} adicionada com sucesso as {DateTime.Now}";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.mensagemErro = ex.Message;
+            }
+
+            return View();
+        }
+
+        // GET: NoticiaController/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var noticia = await _noticaApplication.ObterNoticiaPorId(id);
+
+            return View(noticia);
+        }
+
+        // POST: NoticiaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(NoticiaViewModel noticiaVM)
+        {
+            await _noticaApplication.AtualizarNoticia(noticiaVM);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: NoticiaController/Delete/5
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var noticia = await _noticaApplication.ObterNoticiaPorId(id);
+
+            return View(noticia);
+        }
+
+        // POST: NoticiaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _noticaApplication.DeletarNoticia(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Noticia.MVC/Startup.cs
+++ Noticia.MVC/Startup.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Noticias.Application.Application;
+using Noticias.Application.Interface;
+using Noticias.Domain.Repositories;
+using Noticias.Repo;
+using Noticias.Repo.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,34 +30,42 @@ namespace Noticia.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<NoticiaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddScoped<INoticiaApplication, NoticiaApplication>();
+            services.AddScoped<INoticiaRepository, NoticiaRepository>();
         }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
     }
+}
 }
